b90ffd67bc2e1c27be5f0ffe967862075cbe7a4c
“az” <shimazaki@aaa.email.ne.jp>
2018-10-29 18:41:07 -0400

    edited git ignore

--- download_avatars.js
+++ download_avatars.js
@@ -3,13 +3,23 @@ var request = require('request');
 console.log('welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
-  request(url, function(err, res, body) {
-    cb(err, body);
-  });
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err,body);
+ 
+ });
 }
+
+
 
 getRepoContributors("nodejs", "node", function(err, result){
   console.log("Errors:", err);
   console.log("Result:", result);
 });
+
