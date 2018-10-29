34aea09c57ba27d70c3768f02978884cad324bd2
“az” <shimazaki@aaa.email.ne.jp>
2018-10-29 19:21:26 -0400

    done step7

--- download_avatars.js
+++ download_avatars.js
@@ -3,27 +3,30 @@ var {GITHUB_TOKEN} = require('./secrets');
 
 console.log('welcome to the GitHub Avatar Downloader!');
 
-function getRepoContributors(repoOwner, repoName, cb) {
-  var options = {
-    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-    headers: {
-      'User-Agent': 'request',
-      'token': GITHUB_TOKEN
-    }
-  };
+// function getRepoContributors(repoOwner, repoName, cb) {
+//   var options = {
+//     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+//     headers: {
+//       'User-Agent': 'request',
+//       'token': GITHUB_TOKEN
+//     }
+//   };
 
-  request(options, function(err, res, body) {
-    var obj = JSON.parse(body);
-    cb(err,obj);
- });
+//   request(options, function(err, res, body) {
+//     var obj = JSON.parse(body);
+//     cb(err,obj);
+//  });
+// }
+
+// getRepoContributors("nodejs", "node", function(err, result){
+//   console.log("Errors:", err);
+//   for(obj of result){
+//     console.log("Result:", obj['avatar_url']);
+//   }
+// });
+
+function downloadImageByURL(url, filePath) {
+  // ...
 }
 
-
-
-getRepoContributors("nodejs", "node", function(err, result){
-  console.log("Errors:", err);
-  for(obj of result){
-    console.log("Result:", obj['avatar_url']);
-  }
-});
-
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
