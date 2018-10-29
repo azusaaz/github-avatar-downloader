1d5914de7f24b27458e963a5143b1e6c520f723b
“az” <shimazaki@aaa.email.ne.jp>
2018-10-29 18:17:21 -0400

    done step4

--- download_avatars.js
+++ download_avatars.js
@@ -3,7 +3,10 @@ var request = require('request');
 console.log('welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  
+  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+  request(url, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
 getRepoContributors("nodejs", "node", function(err, result){
