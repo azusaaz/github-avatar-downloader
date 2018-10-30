cfe8767d06f2ade28dc15a5247cbd3653799bd75
“az” <shimazaki@aaa.email.ne.jp>
2018-10-29 20:11:54 -0400

    done step8

--- download_avatars.js
+++ download_avatars.js
@@ -1,32 +1,43 @@
 var request = require('request');
-var {GITHUB_TOKEN} = require('./secrets');
+var {
+  GITHUB_TOKEN
+} = require('./secrets');
+var fs = require('fs');
 
-console.log('welcome to the GitHub Avatar Downloader!');
+console.log('\n========================================');
+console.log('\nwelcome to the GitHub Avatar Downloader!');
 
-// function getRepoContributors(repoOwner, repoName, cb) {
-//   var options = {
-//     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-//     headers: {
-//       'User-Agent': 'request',
-//       'token': GITHUB_TOKEN
-//     }
-//   };
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'token': GITHUB_TOKEN
+    }
+  };
 
-//   request(options, function(err, res, body) {
-//     var obj = JSON.parse(body);
-//     cb(err,obj);
-//  });
-// }
-
-// getRepoContributors("nodejs", "node", function(err, result){
-//   console.log("Errors:", err);
-//   for(obj of result){
-//     console.log("Result:", obj['avatar_url']);
-//   }
-// });
-
-function downloadImageByURL(url, filePath) {
-  // ...
+  request(options, function (err, res, body) {
+    var obj = JSON.parse(body);
+    cb(err, obj);
+  });
 }
 
-downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
+getRepoContributors("nodejs", "node", function (err, result) {
+
+  console.log('\nDownloading images...\n');
+
+  for (obj of result) {
+    downloadImageByURL(obj['avatar_url'], `./avatars/${obj['login']}.jpg`);
+  }
+  console.log('\nDownload complete!\n');
+});
+
+function downloadImageByURL(url, filePath) {
+
+  console.log("filePath", filePath);
+  request.get(url)
+    .on('error', function (err) {
+      throw err;
+    })
+    .pipe(fs.createWriteStream(filePath))
+}
